d610bba3aa28147145fe00130eafec1b8003ae23
yingspvd <supavadeeying@outlook.com>
2021-10-19 23:26:40 +0700

    show all posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -17,7 +17,7 @@ export const createPost = async (req,res) => {
     const newPost = new PostMessage(post)
 
     try {
-      await  newPost.save()
+      await newPost.save()
       res.status(201).json(newPost)
     } catch (error) {
         res.status(409).json({message:error.message})
