c848f7e4bee2b6ff4a0ab8f2f4b5698562ca1bee
yingspvd <supavadeeying@outlook.com>
2021-10-19 21:56:08 +0700

    feat: client side create form and file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+import { getPosts } from "./actions/posts";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Forms/Form";
+import memories from "./images/memories.png";
+import useStyles from "./styles";
 
 const App = () => {
-    return(
-        <div>
-            <h1>App</h1>
-        </div>
-    )
-}
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
---
+++ client/src/actions/posts.js
@@ -0,0 +1,11 @@
+import * as api from "../api";
+
+// Action Creators
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({ type: "FETCH_ALL", payload: [] });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
