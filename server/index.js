61bea589fd878b1cd646a6f17162f4565cfb0166
yingspvd <supavadeeying@outlook.com>
2021-10-20 18:33:40 +0700

    edit url for backend

--- server/index.js
+++ server/index.js
@@ -24,8 +24,6 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
+    app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
   )
   .catch((error) => console.log(`${error} did not connect`));
